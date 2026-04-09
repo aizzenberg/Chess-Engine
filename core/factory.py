6df7052b3a490d862952d54f79e938413885a1c0
aizzenberg <zhirkov.mixail@gmail.com>
2026-04-09 17:03:57 +0100

    Add Positionalist class

--- core/factory.py
+++ core/factory.py
@@ -1,13 +1,17 @@
+from typing import Optional
+
 from engines.materialist import MaterialistEngine
+from engines.positionalist import PositionalistEngine
 
 
-def get_engine(id: str, depth: int, name: str = None):
+def get_engine(id: str, depth: int, **kwargs):
     engines = {
-        'gen1': MaterialistEngine
+        'gen1': MaterialistEngine,
+        'gen2': PositionalistEngine
     }
 
     selected_engine = engines.get(id)
     if not selected_engine:
         raise ValueError(f"Engine '{id}' not found")
 
-    return selected_engine(depth, name)
+    return selected_engine(depth, **kwargs)
