11042a0a0662391ed0d3ce98280a398401b4dc91
aizzenberg <zhirkov.mixail@gmail.com>
2026-06-24 19:00:21 +0100

    Optimise material and positional evaluation using score stack and delta updates

--- core/factory.py
+++ core/factory.py
@@ -1,13 +1,13 @@
 from core.minimax_engine import MinimaxEngine
 from core.move_picker import MovePicker
-from evaluators.materialist import MaterialistEvaluator
-from evaluators.positionalist import PositionalistEvaluator
+from evaluators.materialist import MaterialEvaluator
+from evaluators.positionalist import PositionalEvaluator
 
 
 def get_engine(engine_id: str, depth: int):
     engines = {
-        'gen1': MinimaxEngine(evaluator=MaterialistEvaluator(), move_picker=MovePicker(), depth=depth, name="Beansie"),
-        'gen2': MinimaxEngine(evaluator=PositionalistEvaluator(), move_picker=MovePicker(), depth=depth, name="Tuko"),
+        'gen1': MinimaxEngine(evaluator=MaterialEvaluator(), move_picker=MovePicker(), depth=depth, name="Beansie"),
+        'gen2': MinimaxEngine(evaluator=PositionalEvaluator(), move_picker=MovePicker(), depth=depth, name="Tuko"),
     }
 
     selected_engine = engines.get(engine_id)
